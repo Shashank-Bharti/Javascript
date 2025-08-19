28892e759c89a062d78abfb83ab72b1ccff44649
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-08-19 23:05:36 +0530

    add ES6 class-based inheritance examples and mixins for object inheritance

--- 21.1 oops_inheritance.js
+++ 21.1 oops_inheritance.js
@@ -31,3 +31,68 @@ const myDog = new Dog("buddy")
 
 myDog.bark()
 myDog.speak()
+
+// ES6 Class based Inheritance
+class one {
+    constructor(name) {
+        this.name = name
+    }
+    speaks() {
+        return `my name is ${this.name}`
+    }
+}
+class two extends one {
+    constructor(name) {
+        super(name)
+    }
+}
+const o = new two('Shashank')
+console.log(o.speaks())
+
+// Mixins for inheritance 
+
+const one = {
+    speak() {
+        return `${this.name} speaks`
+    }
+}
+const two = {
+    walks() {
+        return `${this.name} walks`
+    }
+}
+function Person(name) {
+    this.name = name
+}
+Object.assign(Person.prototype, one, two)
+const person1 = new Person('Sudhanshu')
+console.log(person1.speak())
+console.log(person1.walks())
+
+// Inheritance bsed on object.create()method
+
+let obj = {
+    name: 'Sahil',
+    age: 19,
+    prints() {
+        return `my name is ${this.name}`
+    }
+}
+let obj1 = Object.create(obj)
+obj1.name = 'Shivam'
+console.log(obj1.age)
+console.log(obj1.prints())
+
+// Inheritance with object.setPrototypeOf()
+
+const one = {
+    sound(){
+        return `${this.name} barks`
+
+    }
+}
+const two = {
+    name: 'Dog'
+}
+Object.setPrototypeOf(one, two)
+console.log(one.speak())
