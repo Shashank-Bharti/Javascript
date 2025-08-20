2a21cd102c7652029d7c6e53feb71f85a0b78e7e
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-08-20 23:11:41 +0530

    add encapsulation explanation and private variable example

--- 21.2 oops_encapsulation.js
+++ 21.2 oops_encapsulation.js
@@ -1,1 +1,4 @@
-// TBD
+// Encapsulation
+//it is a fundamental concept in object-oriented programming that refers to the practice of hiding the internal details of an object and exposing only the necessary information to the outside world.
+
+// _variable <- this is a private variable can only be accessed inside a close function
