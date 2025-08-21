2dc2775c4ade2f6887f86ff430305e8944780297
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-08-21 23:28:29 +0530

    add detailed comments and examples for polymorphism, method overriding, and overloading

--- 21.3 oops_polymorphism.js
+++ 21.3 oops_polymorphism.js
@@ -1,1 +1,54 @@
-// TBD
+// Polymorphism  - poly - many , morphism - transformations
+// means the same function with different signatures is called many times. 
+// It allows methods to do different things based on the object it is acting upon
+
+// Method Overriding using 'extends'
+
+/*a child class overrides its parent*/
+
+class Vehicle{
+     moves(){
+        console.log('can move on surface,air,water and outerspace');
+     }
+}
+
+class Car extends Vehicle {
+    moves(){
+        console.log('can move on surface');
+        
+    }
+}
+
+class Airplane extends Vehicle {
+    moves(){
+        console.log('can move in air');
+        
+    }
+}
+
+class Boat extends Vehicle {
+    moves(){
+        console.log('can move on water');
+        
+    }
+}
+
+const boat = new Boat()
+const plane = new Airplane()
+boat.moves()
+plane.moves()
+
+// Method Overloading (Compile-time Polymorphism)
+class Calculator {
+    add(a, b) {
+        if (b === undefined) {
+            return a + a; 
+        }
+        return a + b; 
+    }
+}
+
+const calc = new Calculator();
+console.log(calc.add(2)); 
+console.log(calc.add(2, 3));
+
