fbbd2e6c26bee3c47edf4d917abf2f267bdbee36
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-08-27 23:12:23 +0530

    Adds simple fetch API example
    Adds a basic example demonstrating how to use the fetch API
    to retrieve data from a remote endpoint.

---
+++ 23.3 fetch_api.js
@@ -0,0 +1,5 @@
+// simple fetch 
+fetch('https://fakestoreapi.com/products/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
